9458dd176bc126160bbaf190dc3cbdb06b76c4d0
Noah Zarro <zarron@student.ethz.ch>
2018-12-05 19:16:40 +0100

    bot finished

--- Test.py
+++ Test.py
@@ -5,74 +5,66 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from bs4 import BeautifulSoup
 import logging
-import os
-
-page = requests.get("https://attila-teufen.weebly.com/")
-print("Website loaded with code: " + str(page.status_code))
-
-soup = BeautifulSoup(page.content,'html.parser')
-
-subtitles=soup.find_all('h2')
-
-for h2 in subtitles:
-    if (h2.get_text()=="Biberstufe"):
-        biber=h2
-
-biberColum=biber.parent
-
-spalten = biberColum.find_all('td',class_='wsite-multicol-col')
-spalte = spalten[1]
-
-block=spalte.find_all('font')[0]
-
-data_start = str(block).find('>')
-data_end = str(block).find('<',1)
-
-data = str(block)[data_start+1:data_end]
-print(data)
-# data now ready
-
-firstPointIndex = data.find('.')
-secondPointIndex = data.find('.',firstPointIndex+1)
-
-day = int(data[0:firstPointIndex])
-month = int(data[firstPointIndex+1:secondPointIndex])
-year_string = data[secondPointIndex+1:len(data)]
-year = int(year_string)
-
-if len(year_string)==2:
-    year += 2000
 
 
-print(day)
-print(month)
-print(year)
+def parse_website():
+    page = requests.get("https://attila-teufen.weebly.com/")
+    print("Website loaded with code: " + str(page.status_code))
 
-correct=0
+    soup = BeautifulSoup(page.content,'html.parser')
 
-now = datetime.datetime.now()
+    subtitles=soup.find_all('h2')
 
-if datetime.datetime(year,month,day) >= now:
-    correct = 1
+    for h2 in subtitles:
+        if (h2.get_text()=="Biberstufe"):
+            biber=h2
 
-# check day of the week
+    biberColum=biber.parent
 
-#if now.weekday()<3 or now.weekday()==6:
-    #exit(0)
+    spalten = biberColum.find_all('td',class_='wsite-multicol-col')
+    spalte = spalten[1]
+
+    block=spalte.find_all('font')[0]
+
+    data_start = str(block).find('>')
+    data_end = str(block).find('<',1)
+
+    data = str(block)[data_start+1:data_end]
+    print(data)
+    # data now ready
+
+    firstPointIndex = data.find('.')
+    secondPointIndex = data.find('.',firstPointIndex+1)
+
+    day = int(data[0:firstPointIndex])
+    month = int(data[firstPointIndex+1:secondPointIndex])
+    year_string = data[secondPointIndex+1:len(data)]
+    year = int(year_string)
+
+    if len(year_string)==2:
+        year += 2000
+
+    correct=0
+
+    now = datetime.datetime.now()
+
+    if datetime.datetime(year,month,day) >= now:
+        correct = 1
+
+    return correct
 
 # Bot Stuff
 
-telegramBot=0
+
+telegramBot = 0
 chat_ids = set()
 
 # load chats
-subscribers = open("subscribers.txt","r")
+subscribers = open("subscribers.txt", "r")
 for line in subscribers:
     chat_ids.add(int(line))
 
 subscribers.close()
-
-print("fagit")
 
 # create Bot
 TOKEN = '542559791:AAGNLeMRv5qo0zSIpJ8aVUGEtl7-bN5KM5M'
@@ -81,8 +73,8 @@ dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-def start(bot,update):
-    bot.send_message(chat_id=update.message.chat_id, text="Ur an fagit, bot aktiviert")
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Bot aktiviert, du bist auf der Verteilerliste")
     chat_ids.add(update.message.chat_id)
     subscribers_write = open("subscribers.txt", "w")
     for id in chat_ids:
@@ -90,8 +82,8 @@ def start(bot,update):
     subscribers_write.close()
 
 
-def end(bot,update):
-    bot.send_message(chat_id=update.message.chat_id, text="Y u do this, ur an fagit")
+def end(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Bot deaktivert, du bist nicht mehr auf der Verteilerliste")
     chat_ids.remove(update.message.chat_id)
     subscribers_write = open("subscribers.txt", "w")
     for id in chat_ids:
@@ -99,7 +91,7 @@ def end(bot,update):
     subscribers_write.close()
 
 
-start_handler = CommandHandler('start',start)
+start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 end_handler = CommandHandler('end',end)
@@ -110,11 +102,26 @@ updater.start_polling()
 
 time.sleep(5)
 
-if telegramBot != 0:
-    for chat_id in chat_ids:
-        print("message sent to: " + str(chat_id))
-        if correct==0:
+# wake it up every day_to_wake_up at 20:00
 
-            telegramBot.send_message(chat_id=chat_id, text="Die Webseite ist noch nicht aktuell, bitte aktualisieren")
-        else:
-            telegramBot.send_message(chat_id=chat_id, text="Die Webseite ist aktuell, gratuliere")
+day_to_wake_up = 3  # thursday
+
+while 1:
+
+    # check day of the week
+    now = datetime.datetime.now()
+    days_remaining = (day_to_wake_up-datetime.datetime.today().weekday()-1)%7+1
+    wake_up_at = datetime.datetime(now.year, now.month, now.day, 20, 0, 0)
+    time_to_sleep = datetime.timedelta(days_remaining)
+    wake_up_at = wake_up_at + time_to_sleep
+    time.sleep((wake_up_at-now).total_seconds())
+    correct = parse_website()
+
+    if telegramBot != 0:
+        for chat_id in chat_ids:
+            print("message sent to: " + str(chat_id))
+            telegramBot.send_message(chat_id=chat_id, text="Bot erwacht")
+            if correct == 0:
+                telegramBot.send_message(chat_id=chat_id, text="Die Webseite ist noch nicht aktuell, bitte aktualisieren")
+            else:
+                telegramBot.send_message(chat_id=chat_id, text="Die Webseite ist aktuell, gratuliere")
