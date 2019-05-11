b3a7f4d925e48e4aeaad3af66b159e279b3cb21b
zarron <zarron@student.ethz.ch>
2019-05-11 14:50:41 +0200

    finito

--- Python/Test.py
+++ Python/Test.py
@@ -12,7 +12,7 @@ import copy
 activity = []
 activity_atributes = ["Stufe", "Datum", "Typ", "Zeit", "Ort", "Mitnehmen", "Spezielles"]
 to_change = {}
-standard_attribute = {"Stufe": "Biber", "Typ": "Übung", "Datum": "df", "Zeit": "14:00 - 16:00", "Ort":  "Pfadiheim", "Mitnehmen":  "", "Spezielles": ""}
+standard_attribute = {"Stufe": "Biber", "Typ": "Übung", "Datum": "df", "Zeit": "14:00 - 16:00", "Ort":  "Pfadiheim", "Mitnehmen":  "-", "Spezielles": "-"}
 
 # load activity
 with open("activity.json","r") as read_file:
@@ -21,6 +21,10 @@ with open("activity.json","r") as read_file:
 # Bot Stuff
 telegramBot = 0
 chat_ids = set()
+
+# ok
+ok = {"Biber": "nope", "Wölfe": "nope", "Pfader": "nope"}
+ok_alias = {"nope": "Nicht Aktuell", "ok": "Aktuell", "later": "Keine Übung"}
 
 # next message
 next_message = {}
@@ -38,6 +42,28 @@ with open("token.json","r") as read_file:
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def send_to_server():
+    # store activity
+    with open("activity.json", "w") as write_file:
+        json.dump(activity, write_file)
+
+    url = "http://people.ee.ethz.ch/~zarron/attila/attila_api_send.php"
+
+    # load passwort
+    with open("password.json", "r") as read_file:
+        password = json.load(read_file)[0]
+
+    with open("activity.json", "r") as read_file:
+        payload_string = json.dumps(json.load(read_file))
+
+    payload = {"load": payload_string, "password": password}
+
+    try:
+        requests.post(url, data=payload)
+    except:
+        print("failed")
 
 
 def write_people():
@@ -172,36 +198,57 @@ def inline_handler(bot, update):
         attribute = to_change[query.message.chat_id].pop(0)
         keyboard = []
         row = []
-        row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, standard_attribute[attribute], attribute)))
-        row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, "Eingeben", attribute)))
-        row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, "Abbrechen", attribute)))
+        row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, standard_attribute[attribute])))
+        row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, "Eingeben")))
+        row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, "Abbrechen")))
         keyboard.append(row)
 
         bot.send_message(chat_id=query.message.chat_id, text=attribute, reply_markup=InlineKeyboardMarkup(keyboard))
 
     if action == "change_activity_atribute":
-        if value == "Abbrechen":
-            # could reset to_change, but does not matter (hopefully)
-            return
+        print("Arg: " + arg)
+        if arg == "Abbrechen":
+            to_change[query.message.chat_id] = []
 
-        if value == "Eingeben":
-            next_message[query.message.chat_id] = "{} {}".format("Attribut", arg)
-            return
+        if arg == "Eingeben":
+            #set_activity_element(get_level(query.message.chat_id), value, standard_attribute[value])
+            next_message[query.message.chat_id] = "{} {}".format("Attribut", value)
+            bot.send_message(chat_id=query.message.chat_id, text=value + " eingeben:")
 
-        if len(to_change[query.message.chat_id]) > 0 and value != "Abbrechen" and value != "Eingeben":
-            print("change last activity")  # change last activity
+        if arg != "Abbrechen" and arg != "Eingeben":
+            # change last activity
 
+            set_activity_element(get_level(query.message.chat_id), value, standard_attribute[value])
 
+            if len(to_change[query.message.chat_id]) == 0:
+                current_with_id(bot, query.message.chat_id)
 
-            attribute = to_change[query.message.chat_id].pop(0)
-            keyboard = []
-            row = []
-            row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, standard_attribute[attribute], attribute)))
-            row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, "Eingeben", attribute)))
-            row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, "Abbrechen", attribute)))
-            keyboard.append(row)
+            if len(to_change[query.message.chat_id]) > 0:
+                attribute = to_change[query.message.chat_id].pop(0)
+                keyboard = []
+                row = []
+                row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, standard_attribute[attribute])))
+                row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, "Eingeben")))
+                row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, attribute, "Abbrechen")))
+                keyboard.append(row)
 
-            bot.send_message(chat_id=query.message.chat_id, text=attribute, reply_markup=InlineKeyboardMarkup(keyboard))
+                bot.send_message(chat_id=query.message.chat_id, text=attribute, reply_markup=InlineKeyboardMarkup(keyboard))
+        send_to_server()
+
+    if action == "change_single_atribute":
+        to_change[query.message.chat_id] = []
+        keyboard = []
+        row = []
+        row.append(InlineKeyboardButton(standard_attribute[value], callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, value, standard_attribute[value])))
+        row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, value, "Eingeben")))
+        row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", query.message.chat_id, value, "Abbrechen")))
+        keyboard.append(row)
+
+        bot.send_message(chat_id=query.message.chat_id, text=value, reply_markup=InlineKeyboardMarkup(keyboard))
+
+    if action == "change_status":
+        ok[arg] = value
+        bot.send_message(chat_id=query.message.chat_id, text="Status ist jetzt: " + ok_alias[value])
 
 
 def reminder(weekday):
@@ -209,28 +256,30 @@ def reminder(weekday):
     for person in people:
         print(person["ID"], " ", person[weekday])
         if person[weekday] == "Ja":
-            updater.bot.send_message(chat_id=person["ID"], text="Reminder")
+            if ok[person["level"]] == "nope":
+                updater.bot.send_message(chat_id=person["ID"], text="Reminder")
 
 
 def insult():
-    return ""#"Vollasichlotz"
+    return "Vollasichlotz"
 
 
 def get_level(id):
     id = str(id)
     for person in people:
         if person["ID"] == id:
-            print(person["level"])
             return person["level"]
 
 
 def set_activity_element(level, key, value):
     i = 0
-    while i<len(activity):
-        if activity[i]["Stufe"] == level:
-            activity[i][key] = value
-            return
+    local_activity = activity
+    while i < len(local_activity):
+        if local_activity[i]["Stufe"] == level:
+            local_activity[i][key]
+            local_activity[i][key] = value
         i += 1
+    activity[:] = local_activity
 
 
 def get_activity_element(level, key):
@@ -255,6 +304,14 @@ def current(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Zerst mit \start registriere, " + insult())
 
 
+def current_with_id(bot, id):
+    level = get_person_value(id, "level")
+    if level is not None:
+        bot.send_message(chat_id=id, text=get_activity(level), parse_mode="HTML")
+    else:
+        bot.send_message(chat_id=id, text="Zerst mit \start registriere, " + insult())
+
+
 def change_activity(bot, update):
     current(bot, update)
     keyboard = []
@@ -264,14 +321,12 @@ def change_activity(bot, update):
     for atribute in activity_atributes:
         if atribute != "Stufe":
             row = list()
-            row.append(InlineKeyboardButton(atribute, callback_data="{} {} {}".format("change_activity_atribute", update.message.chat_id, atribute)))
+            row.append(InlineKeyboardButton(atribute, callback_data="{} {} {}".format("change_single_atribute", update.message.chat_id, atribute)))
             keyboard.append(row)
     bot.send_message(chat_id=update.message.chat_id, text="Was möchtest du bearbeiten?", reply_markup=InlineKeyboardMarkup(keyboard))
 
 
 def answer_handler(bot, update):
-    print("gotter")
-    print(next_message[update.message.chat_id])
     if not update.message.chat_id in next_message:
         bot.send_message(chat_id=update.message.chat_id, text="Verstandi nöd, " + insult())
         return
@@ -283,7 +338,6 @@ def answer_handler(bot, update):
     if len(data.split(" ")) > 1:
         arg = data.split(" ")[1]
 
-    print(command)
 
     # reset type of next expected answer
     next_message[update.message.chat_id] = ""
@@ -299,20 +353,47 @@ def answer_handler(bot, update):
 
     elif command == "Attribut":
         set_activity_element(get_level(update.message.chat_id), arg, update.message.text)
+
+        if len(to_change[update.message.chat_id]) == 0:
+            current(bot, update)
+
         # issue new request
         if len(to_change[update.message.chat_id]) > 0:
             attribute = to_change[update.message.chat_id].pop(0)
             keyboard = []
             row = []
-            row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {}".format("change_activity_atribute", update.message.chat_id, standard_attribute[attribute], attribute)))
-            row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {}".format("change_activity_atribute", update.message.chat_id, "Eingeben", attribute)))
-            row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {}".format("change_activity_atribute", update.message.chat_id, "Abbrechen", attribute)))
+            row.append(InlineKeyboardButton(standard_attribute[attribute], callback_data="{} {} {} {}".format("change_activity_atribute", update.message.chat_id, attribute, standard_attribute[attribute])))
+            row.append(InlineKeyboardButton("Eingeben", callback_data="{} {} {} {}".format("change_activity_atribute", update.message.chat_id, attribute, "Eingeben")))
+            row.append(InlineKeyboardButton("Abbrechen", callback_data="{} {} {} {}".format("change_activity_atribute", update.message.chat_id, attribute, "Abbrechen")))
             keyboard.append(row)
 
             bot.send_message(chat_id=update.message.chat_id, text=attribute, reply_markup=InlineKeyboardMarkup(keyboard))
+        send_to_server()
 
     else:
         bot.send_message(chat_id=update.message.chat_id, text="Verstandi nöd " + insult() + ", benutz en Command")
+
+
+def reset(gitter):
+    print("reset")
+    global  ok
+    ok.clear()
+    ok = {"Biber": "nope", "Wölfe": "nope", "Pfader": "nope"}
+    standard_attribute["Datum"] = events.get_next_saturday()
+
+def change_state(bot, update):
+    level = get_level(update.message.chat_id)
+    status = ok_alias[ok[level]]
+
+    keyboard = []
+    row = []
+    row.append(InlineKeyboardButton("Aktuell", callback_data="{} {} {} {}".format("change_status", update.message.chat_id, "ok", level)))
+    row.append(InlineKeyboardButton("Nicht Aktuell", callback_data="{} {} {} {}".format("change_status", update.message.chat_id, "nope", level)))
+    row.append(InlineKeyboardButton("Keine Übung", callback_data="{} {} {} {}".format("change_status", update.message.chat_id, "later", level)))
+    keyboard.append(row)
+
+    bot.send_message(chat_id=update.message.chat_id, text="Status: " + status + ". Ändern?", reply_markup=InlineKeyboardMarkup(keyboard))
+
 
 
 # register commands
@@ -321,6 +402,7 @@ dispatcher.add_handler(CommandHandler('settings', settings))
 dispatcher.add_handler(CommandHandler('info', info))
 dispatcher.add_handler(CommandHandler('current', current))
 dispatcher.add_handler(CommandHandler('change', change_activity))
+dispatcher.add_handler(CommandHandler('set_state', change_state))
 
 
 # register inline query handler
@@ -332,14 +414,20 @@ dispatcher.add_handler(MessageHandler(Filters.text, answer_handler))
 telegramBot = updater.bot
 updater.start_polling()
 
+
 reminder_Mo = events.TimedEvent(0, 10, 0, reminder, "Mo")
 reminder_Do = events.TimedEvent(1, 17, 15, reminder, "Do")
 reminder_Fr = events.TimedEvent(4, 17, 0, reminder, "Fr")
 reminder_Sa = events.TimedEvent(5, 11, 0, reminder, "Sa")
+reset_Sa = events.TimedEvent(5, 18, 0, reset, "Sa")
 
 print("bot started")
+reset("lehm")
 
 while True:
+    reset_Sa.wait()
+    reminder_Mo.wait()
     reminder_Do.wait()
     reminder_Fr.wait()
     reminder_Sa.wait()
+
